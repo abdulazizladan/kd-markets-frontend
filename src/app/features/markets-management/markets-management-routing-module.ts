9b92131c76aa8a8790de665ce0420b5199e932ce
abdulazizladan <abdulazizladan@gmail.com>
2025-08-26 16:29:46 +0100

    task: Enable routes for markets and users

--- src/app/features/markets-management/markets-management-routing-module.ts
+++ src/app/features/markets-management/markets-management-routing-module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MarketsListComponent } from './components/markets-list-component/markets-list-component';
+import { MarketDetailsComponent } from './components/market-details-component/market-details-component';
 
 const routes: Routes = [
   {
     path: "",
     component: MarketsListComponent
+  },
+  {
+    path: "market/:id",
+    component: MarketDetailsComponent
   },
   {
     path: "**",
--- src/app/features/payments-management/payments-management-routing-module.ts
+++ src/app/features/payments-management/payments-management-routing-module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PaymentsSummaryComponent } from './components/payments-summary-component/payments-summary-component';
+import { PaymentHistoryComponent } from './components/payment-history-component/payment-history-component';
 
 const routes: Routes = [
   {
     path: '',
     component: PaymentsSummaryComponent
+  },
+  {
+    path: 'history',
+    component: PaymentHistoryComponent
   }
 ];
 
